8e9ebd474c6762eb5ab977477db36b8df74ba7ed
ZyvTheBatchCoder <92561511+ZyvTheBatchCoder@users.noreply.github.com>
2021-10-14 21:13:39 -0600

    Add files via upload
    Super easy to use just open it up and boom it will start rickrolling you.

---
+++ rickroll.bat
@@ -0,0 +1,28 @@
+@echo off
+Title Zyv's RickRoll Rainbow Edition
+:top
+color 04
+echo we are no strangers to love
+color 01
+echo you know the rules and so do i
+color 02
+echo a full commitment is what im thinking off
+color 05
+echo never gonna give you up
+echo never gonna let you down
+color 06
+echo never gonna let you down
+color 07
+echo never gonna run around a desrt you
+goto top
+
+
+:4loop
+echo we are no strangers to love
+echo you know the rules and so do i
+echo a full commitment is what im thinking off
+echo you wouldent get this from anyother guy
+echo never gonna give you up
+echo never gonna let you down
+echo never gonna run around a desrt you
+Goto :4loop
